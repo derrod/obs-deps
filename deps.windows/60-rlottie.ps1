c34e707a4cca5a3dec74f42306e3542632b2524e
derrod <xlnedder@gmail.com>
2023-05-20 22:04:22 +0200

    deps.windows: Add rlottie

---
+++ deps.windows/60-rlottie.ps1
@@ -0,0 +1,65 @@
+param(
+    [string] $Name = 'rlottie',
+    [string] $Version = 'V0.2-modified',
+    [string] $Uri = 'https://github.com/Samsung/rlottie.git',
+    [string] $Hash = '056c0a1bc14bcee6f6b1a821013872a1228b0fa9'
+)
+
+function Setup {
+    Setup-Dependency -Uri $Uri -Hash $Hash -DestinationPath $Path
+}
+
+function Clean {
+    Set-Location $Path
+
+    if ( Test-Path "build_${Target}" ) {
+        Log-Information "Clean build directory (${Target})"
+        Remove-Item -Path "build_${Target}" -Recurse -Force
+    }
+}
+
+function Configure {
+    Log-Information "Configure (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        $CmakeOptions
+        '-DBUILD_SHARED_LIBS=OFF'
+        '-DLOTTIE_MODULE=OFF'
+        '-DLIB_INSTALL_DIR=lib'
+    )
+
+    Invoke-External cmake -S . -B "build_${Target}" @Options
+}
+
+function Build {
+    Log-Information "Build (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--build', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $VerbosePreference -eq 'Continue' ) {
+        $Options += '--verbose'
+    }
+
+    Invoke-External cmake @Options
+}
+
+function Install {
+    Log-Information "Install (${Target})"
+    Set-Location $Path
+
+    $Options = @(
+        '--install', "build_${Target}"
+        '--config', $Configuration
+    )
+
+    if ( $Configuration -match "(Release|MinSizeRel)" ) {
+        $Options += '--strip'
+    }
+
+    Invoke-External cmake @Options
+}
