a33eb5475d2f8d66abf24bab9d63cef3e6b7937d
derrod <xlnedder@gmail.com>
2023-09-03 05:40:31 +0200

    qt: Add patch to fix Windows style

--- deps.qt/qt6.ps1
+++ deps.qt/qt6.ps1
@@ -3,7 +3,13 @@ param(
     [string] $Version = '6.5.2',
     [string] $Uri = 'https://download.qt.io/official_releases/qt/6.5/6.5.2',
     [string] $Hash = "${PSScriptRoot}/checksums",
-    [array] $Targets = @('x64', 'x86')
+    [array] $Targets = @('x64', 'x86'),
+    [array] $Patches = @(
+        @{
+            PatchFile = "${PSScriptRoot}/patches/0001-fix-vista-style-clobbering-stylesheets.diff"
+            HashSum = "c2cc0c114c09eb29d0a57b0adbd3ab4a2a1204fe92bc1e75e1e2e94e3e037fb7"
+        }
+    )
 )
 
 $QtComponents = @(
